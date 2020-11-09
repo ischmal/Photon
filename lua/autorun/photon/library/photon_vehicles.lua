687f7e5d9962b59efaaf702e38cbf31d6487e7fd
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2020-11-09 15:01:22 +0000

    Index the scripts.

--- lua/autorun/photon/library/photon_vehicles.lua
+++ lua/autorun/photon/library/photon_vehicles.lua
@@ -55,6 +55,7 @@ local PhotonEMVIndex = {
 	["models/schmal/chev_tahoe16.mdl"] = "2016 Chevrolet Tahoe PPV",
 	["models/tdmcars/emergency/for_taurus_13.mdl"] = "Ford Taurus 2013",
 	["models/lonewolfie/dodge_charger_2015_police.mdl"] = "Dodge Charger R/T 2015 Pursuit",
+	["models/lonewolfie/volvo_s60_pol.mdl"] = "Volvo S60 R Police"
 	-- ["models/talonvehicles/tal_chev_suburban_2015.mdl"] = "2015 Chevrolet Suburban"
 }
 
@@ -124,7 +125,8 @@ local indexed_emvs = {
 	"default_schmal_fpiu16",
 	"default_schmal_tahoe16",
 	"default_tdm_fpis",
-	"default_lw_charger"
+	"default_lw_charger",
+	"default_lw_volvo_s60"
 	-- "default_talon_suburban"
 }
 
