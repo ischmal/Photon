b82ef8b3f48ec044e3f983b11a3b2d14797aa4c9
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2020-11-12 18:53:42 +0000

    Remove the colours we don't use.

--- lua/autorun/photon/library/default/default_lw_volvo_s60.lua
+++ lua/autorun/photon/library/default/default_lw_volvo_s60.lua
@@ -25,11 +25,7 @@ local function MirrorAngle(ang, method)
 	return newAng
 end
 
-local A = "AMBER"
-local R = "RED"
-local DR = "D_RED"
 local B = "BLUE"
-local W = "WHITE"
 local CW = "C_WHITE"
 local SW = "S_WHITE"
 
